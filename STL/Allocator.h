5f7f1a6ac94d7e5c2bdb1ed879e26e793118cbb6
leo sher <42842564+dasklarleo@users.noreply.github.com>
2021-01-18 11:05:32 +0800

    fix bug

--- STL/Allocator.h
+++ STL/Allocator.h
@@ -1,6 +1,9 @@
 //
 // Created by 刘玉晓 on 2021-01-17.
 //
+//所有的内存分配一级级往下最终都是到达malloc层面
+//但是malloc由于会在内存之中记载cookie 大小等信息
+//因此对小内存的需求进行内存分配时频繁使用malloc会引起较大的浪费
 
 #ifndef STL_ALLOCATOR_H
 #define STL_ALLOCATOR_H
@@ -11,65 +14,129 @@
 #include <iostream>
 
 
-template<int inst>
-class MallocAllocTemplate {
+template<int inst>//inst区分类模版和类
+class MallocAllocTemplate {//只有需要分配的内存大于128字节时才会调用此class内的函数
+    //这也就是第二级配置器
 private:
-    static void *SOOMMalloc(size_t n) {
-        void *result;
-        void (* TempHandler)();
-        for (;;) {
-            TempHandler=MallocAllocOOMHandler;
-            if (0 == TempHandler) {
-                std::cerr << "NO ENOHGH MEMORY" << "\n";
-                exit(1);
-            }
-            (* TempHandler)();//????
-            result = malloc(n);
-            if (result)
-                return result;
-        }
-    }
-
-    static void *SOOMRealloc(void *p,size_t n) {
-        void *result;
-        void (* TempHandler)();
-        for (;;) {
-            TempHandler=MallocAllocOOMHandler;
-            if (0 == TempHandler) {
-                std::cerr << "NO ENOHGH MEMORY" << "\n";
-                exit(1);
-            }
-            (* TempHandler)();//????
-            result = malloc(n);
-            if (result)
-                return result;
-        }
-    }
-
-    static void (*MallocAllocOOMHandler)();
 
 public:
-    static void * allocate(size_t n){
-        void * result=malloc(n);
-        if(0==result)
-            result=SOOMMalloc(n);
+    static void *allocate(size_t n) {
+        void *result = malloc(n);
+        if (0 == result)
+            std::cerr << "NO ENOUGH MEMORY" << "\n";
         return result;
     }
-    static void deallocate(void *p ,size_t){
+
+    static void deallocate(void *p, size_t) {
         free(p);
     }
-    static void* reallocate(void* p, size_t , size_t n)
-    {
-        void* result = realloc(p, n);
-        if (0 == result) result = SOOMRealloc(p, n);
+
+    static void *reallocate(void *p, size_t, size_t n) {
+        void *result = realloc(p, n);
+        if (0 == result)
+            std::cerr << "NO ENOUGH MEMORY" << "\n";
         return result;
     }
-    static void (* SetMallocHandler(void (*__f)()))()
-    {
-        void (* old)() = MallocAllocOOMHandler;
-        MallocAllocOOMHandler = __f;
-        return(old);
+
+};
+
+typedef MallocAllocTemplate<0> MallocAlloc;
+
+template<int inst>
+class DefaultAlloc {
+private:
+    enum {
+        ALIGN = 8
+    };//128字节为分界线
+    enum {
+        MAX_BYTES = 128
+    };//以8个字节为单位分配
+    enum {
+        _NFREELISTS = 16
+    };//位数16组链表
+    static size_t RoundUp(size_t bytes) {
+        return ((bytes + ALIGN) / ALIGN) * ALIGN;
     }
+
+    union Obj {
+        union Obj *MemFreeListLink;
+        char MemClientData[1];
+    };
+
+    static Obj *SFreeList[];
+
+    static size_t SFreeListIndex(size_t bytes) {//找到要在8条链表之中的哪一条取内存块
+        return (((bytes) + ALIGN - 1) / ALIGN - 1);
+    }
+
+    //这个n一定是8的整数倍数
+    static void *SRefill(size_t size) {
+        int num = 20;   //一次想要20块
+        char *chunk = SChunkAlloc(size, num);//num传的是引用,返回真正分配了的内存块数量
+        Obj **MyfreeList;
+        Obj *result;
+        Obj *CurrentObj;
+        Obj *NextObj;
+        if (1 == num)
+            return chunk;
+        else {// 切成一个个的大小为size的小块加入freelist
+            result = (Obj *) chunk;
+            MyfreeList = SFreeList + SFreeListIndex(size);//找到了对应的链表
+            NextObj = (Obj *) (chunk + size);//因为大小是size
+            *MyfreeList = NextObj;
+            for (int i = 1;; i++) {
+                CurrentObj = NextObj;
+                NextObj = (Obj *) ((char *) NextObj + size);
+                if (num - 1 == i) {
+                    CurrentObj->MemFreeListLink = 0;
+                    break;
+                } else {
+                    CurrentObj->MemFreeListLink = NextObj;
+                }
+            }
+            return result;
+        }
+
+    }
+
+    static char *SChunkAlloc(size_t size, int &n) {
+        char *result;
+        size_t TotalBytes = size * n;
+        size_t BytesLeft = SEndFree - SStartFree;
+        if (BytesLeft >= TotalBytes) {//可以分配这么多的内存
+            result = SStartFree;
+            SStartFree += TotalBytes;
+            return result;
+        } else {
+            size_t BytesToGet = TotalBytes * 2 + RoundUp(SHeapSize * 16);
+            if (BytesLeft > 0) {
+                Obj** MyfreeList=SFreeList + SFreeListIndex(BytesLeft);//找到了对应的链表
+                
+
+            }
+        }
+    }
+
+    // 记录分配的状态,内存池起始位置和结束位置
+    static char *SStartFree;
+    static char *SEndFree;
+    static size_t SHeapSize;
+public:
+    static void *Allocate(size_t n) {
+
+        if (n > MAX_BYTES) {
+            return (MallocAlloc::allocate(n));
+        } else {
+            Obj **MyFreeList = SFreeList + SFreeListIndex(n);//确定空闲链表
+            Obj *result = *MyFreeList;
+            if (result == 0)//没有空闲的了,尝试再要点内存
+                result = SRefill(RoundUp(n));
+            else
+                result = result->MemFreeListLink;
+            return result;
+        }
+    }
+
 };
 
 
@@ -102,38 +169,7 @@ namespace Allocator {
 
     template<class T>
     class allocator {
-    private:
-        enum {
-            ALIGN = 8
-        };//128字节为分界线
-        enum {
-            MAX_BYTES = 128
-        };//以8个字节为单位分配
-        enum {
-            _NFREELISTS = 16
-        };//位数16组链表
-        static size_t RoundUp(size_t bytes) {
-            return ((bytes + ALIGN) / ALIGN) * ALIGN;
-        }
 
-        union Obj {
-            union Obj *MemFreeListLink;
-            char MemClientData[1];
-        };
-
-        static size_t SFreeListIndex(size_t bytes) {
-            return (((bytes) + ALIGN - 1) / ALIGN - 1);
-        }
-
-        //后续可能要使用的函数
-        static void *SRefill(size_t n) {}
-
-        static char *S_chunk_alloc(size_t size, int &nobjs);
-
-        // Chunk allocation state.
-        static char *S_StartFree;
-        static char *SEndFree;
-        static size_t SHeapSize;
     public:
         typedef T ValueType;
         typedef T *Pointer;
