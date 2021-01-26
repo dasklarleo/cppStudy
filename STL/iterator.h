8c1da20d14809382c2b823520a18825b44ef2c31
leo sher <42842564+dasklarleo@users.noreply.github.com>
2021-01-27 00:03:36 +0800

    keep adding

--- STL/iterator.h
+++ STL/iterator.h
@@ -9,16 +9,63 @@
 
 #endif //STL_ITERATOR_H
 
-struct InputInteratorTag {
+struct InputIteratorTag {
 };
 struct OutputIteratorTag {
 };
-struct ForwardIteratorTag : public InputInteratorTag {
+struct ForwardIteratorTag : public InputIteratorTag {
 };
 struct BidirectionalIteratorTag : public ForwardIteratorTag {
 };
 struct RandomAccessIteratorTag : public BidirectionalIteratorTag {
 };
+
+template<class T, class Distance>
+struct InputIterator {
+    typedef InputIteratorTag iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef T *pointer;
+    typedef T &reference;
+};
+
+struct OutputIterator {
+    typedef OutputIteratorTag iterator_category;
+    typedef void value_type;
+    typedef void difference_type;
+    typedef void pointer;
+    typedef void reference;
+};
+
+template<class T, class Distance>
+struct ForwardIterator {
+    typedef ForwardIteratorTag iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef T *pointer;
+    typedef T &reference;
+};
+
+
+template<class T, class Distance>
+struct BidirectionalIterator {
+    typedef BidirectionalIteratorTag iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef T *pointer;
+    typedef T &reference;
+};
+
+template<class T, class Distance>
+struct RandomAccessIterator {
+    typedef RandomAccessIteratorTag iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef T *pointer;
+    typedef T &reference;
+};
+
+
 /*
  * 上述迭代器应用待补充
  * */
@@ -43,6 +90,7 @@ struct IteratorTraits {
     typedef typename itertaor::difference_type difference_type;
     typedef typename itertaor::pointer pointer;
     typedef typename itertaor::reference reference;
+
 };
 
 template<class T>
@@ -62,7 +110,133 @@ struct IteratorTraits<const T *> {
     typedef T *pointer;
     typedef T &reference;
 };
+//tarits编程结束
 
+
+/*
+ * 为五种迭代器单独设置函数
+ * */
+//1 返回类别函数
+template<class T, class Distance>
+inline InputIteratorTag
+ItearotCategory(const InputIterator<T, Distance> &) { return InputIteratorTag(); }
+
+inline OutputIteratorTag iterator_category(const OutputIterator &) { return OutputIteratorTag(); }
+
+template<class T, class Distance>
+inline ForwardIteratorTag
+ItearotCategory(const ForwarIterator <T, Distance> &) { return ForwardIteratorTag(); }
+
+template<class T, class Distance>
+inline BidirectionalIteratorTag
+ItearotCategory(const BidirectionalIterator<T, Distance> &) { return BidirectionalIteratorTag(); }
+
+template<class T, class Distance>
+inline RandomAccessIteratorTag
+ItearotCategory(const RandomAccessIterator<T, Distance> &) { return RandomAccessIteratorTag(); }
+
+template<class T>
+inline RandomAccessIteratorTag ItearotCategory(const T *) { return RandomAccessIteratorTag(); }
+//1 返回类别函数结束
+
+//2 返回值的类型
+template<class T, class Distance>
+inline T *ValueType(const InputIterator<T, Distance> &) { return (T *) (0); }
+
+template<class T, class Distance>
+inline T *ValueType(const ForwarIterator <T, Distance> &) { return (T *) (0); }
+
+template<class T, class Distance>
+inline T *ValueType(const BidirectionalIterator<T, Distance> &) { return (T *) (0); }
+
+template<class T, class Distance>
+inline T *ValueType(const RandomAccessIterator<T, Distance> &) { return (T *) (0); }
+
+template<class T>
+inline T *ValueType(const T *) { return (T *) (0); }
+//2 返回值函数结束
+
+
+//3 距离类型函数
+template<class T, class Distance>
+inline Distance *DistanceType(const InputIterator<T, Distance> &) {
+    return (Distance *) (0);
+}
+
+template<class T, class Distance>
+inline Distance *DistanceType(const ForwarIterator <T, Distance> &) {
+    return (Distance *) (0);
+}
+
+template<class T, class Distance>
+inline Distance *
+DistanceType(const BidirectionalIterator<T, Distance> &) {
+    return (Distance *) (0);
+}
+
+template<class T, class Distance>
+inline Distance *
+DistanceType(const RandomAccessIterator<T, Distance> &) {
+    return (Distance *) (0);
+}
+
+template<class T>
+inline ptrdiff_t *DistanceType(const T *) { return (ptrdiff_t *) (0); }
+//3 距离类型函数结束
+
+//4 距离大小函数
+template<class InputIterator>
+inline typename IteratorTraits<InputIterator>::difference_type
+DistanceInner(InputIterator first, InputIterator last, InputIteratorTag) {
+    typename IteratorTraits<InputIterator>::difference_type n = 0;
+    while (first != last) {
+        ++first;
+        ++n;
+    }
+    return n;
+}
+
+template<class RandomIterator>
+inline typename IteratorTraits<RandomIterator>::difference_type
+DistanceInner(RandomIterator first, RandomIterator last, RandomAccessIteratorTag) {
+    return last - first;
+}
+
+template<class InputIterator>
+inline typename IteratorTraits<InputIterator>::difference_type
+Distance(InputIterator first, InputIterator last) {
+    typedef typename IteratorTraits<InputIterator>::iterator_category category;
+    return DistanceInner(first, last, category());
+}
+//4 距离大小函数结束
+
+//5 前进函数
+template<class BidirectionalIterator, class Distance>
+inline void AdvanceInner(BidirectionalIterator &i, Distance n, BidirectionalIteratorTag) {
+    if (n >= 0)
+        while (n--)++i;
+    else
+        while (n++)--i;
+}
+
+template<class InputIterator, class Distance>
+inline void AnvanceInner(InputIterator &i, Distance n, InputIteratorTag) {
+    while (n--)++i;
+}
+
+template<class RandomAccessIterator, class Distance>
+inline void AnvanceInner(RandomAccessIterator &i, Distance n, RandomAccessIteratorTag) {
+    i += n;
+}
+
+template<class InputIterator, class Distance>
+inline void Advance(InputIterator &i, Distance n) {
+    AdvanceInner(i, n, IteratorCategory(i));
+}
+//5 前进函数结束
+
+
+//
 /*
  * 重载
  */
@@ -86,49 +260,205 @@ ValueType(const Iter &) {
 }
 
 
-template<class InputInterator>
-inline typename IteratorTraits<InputInterator>::difference_type
-DistanceInner(InputInterator first, InputInterator last, InputInteratorTag) {
-    typename IteratorTraits<InputInterator>::difference_type n = 0;
-    while (first != last) {
-        ++first;
-        ++n;
+
+
+/*
+ * 为不同类型迭代器设置重载*/
+
+//1 后插类型
+template<class Container>
+class BackInsertIterator {
+protected:
+    Container *container;
+public:
+    typedef Container container_type;
+    typedef OutputIteratorTag iterator_category;
+    typedef void value_type;
+    typedef void difference_type;
+    typedef void pointer;
+    typedef void reference;
+
+    explicit BackInsertIterator(Container &__x) : container(&__x) {}
+
+    BackInsertIterator<Container> &
+    operator=(const typename Container::value_type &__value) {
+        container->push_back(__value);
+        return *this;
     }
-    return n;
+
+    BackInsertIterator<Container> &operator*() { return *this; }
+
+    BackInsertIterator<Container> &operator++() { return *this; }
+
+    BackInsertIterator<Container> &operator++(int) { return *this; }
+};
+//1 后插类型结束
+
+//2 前插类型
+template<class Container>
+class FrontInsertIterator {
+protected:
+    Container *container;
+public:
+    typedef Container container_type;
+    typedef OutputIteratorTag iterator_category;
+    typedef void value_type;
+    typedef void difference_type;
+    typedef void pointer;
+    typedef void reference;
+
+    explicit FrontInsertIterator(Container &__x) : container(&__x) {}
+
+    FrontInsertIterator<Container> &
+    operator=(const typename Container::value_type &__value) {
+        container->push_front(__value);
+        return *this;
+    }
+
+    FrontInsertIterator<Container> &operator*() { return *this; }
+
+    FrontInsertIterator<Container> &operator++() { return *this; }
+
+    FrontInsertIterator<Container> &operator++(int) { return *this; }
+};
+
+//2 前插类型结束
+
+//3 插入类型
+template<class Container>
+class InsertIterator {
+protected:
+    Container *container;
+    typename Container::iterator iter;
+public:
+    typedef Container container_type;
+    typedef OutputIteratorTag iterator_category;
+    typedef void value_type;
+    typedef void difference_type;
+    typedef void pointer;
+    typedef void reference;
+
+    InsertIterator(Container &__x, typename Container::iterator __i)
+            : container(&__x), iter(__i) {}
+
+    InsertIterator<Container> &
+    operator=(const typename Container::value_type &__value) {
+        iter = container->insert(iter, __value);
+        ++iter;
+        return *this;
+    }
+
+    InsertIterator<Container> &operator*() { return *this; }
+
+    InsertIterator<Container> &operator++() { return *this; }
+
+    InsertIterator<Container> &operator++(int) { return *this; }
+};
+
+//3 插入类型结束
+//4 双向插入
+template<class Iterator>
+class reverse_iterator {
+protected:
+    Iterator current;
+public:
+    typedef typename IteratorTraits<Iterator>::iterator_category
+            iterator_category;
+    typedef typename IteratorTraits<Iterator>::value_type
+            value_type;
+    typedef typename IteratorTraits<Iterator>::difference_type
+            difference_type;
+    typedef typename IteratorTraits<Iterator>::pointer
+            pointer;
+    typedef typename IteratorTraits<Iterator>::reference
+            reference;
+
+    typedef Iterator iterator_type;
+    typedef reverse_iterator<Iterator> _Self;
+
+public:
+    reverse_iterator() {}
+
+    explicit reverse_iterator(iterator_type __x) : current(__x) {}
+
+    reverse_iterator(const _Self &__x) : current(__x.current) {}
+
+
+    iterator_type base() const { return current; }
+
+    reference operator*() const {
+        Iterator __tmp = current;
+        return *--__tmp;
+    }
+
+    pointer operator->() const { return &(operator*()); }
+
+    _Self &operator++() {
+        --current;
+        return *this;
+    }
+
+    _Self operator++(int) {
+        _Self __tmp = *this;
+        --current;
+        return __tmp;
+    }
+
+    _Self &operator--() {
+        ++current;
+        return *this;
+    }
+
+    _Self operator--(int) {
+        _Self __tmp = *this;
+        ++current;
+        return __tmp;
+    }
+
+    _Self operator+(difference_type __n) const {
+        return _Self(current - __n);
+    }
+
+    _Self &operator+=(difference_type __n) {
+        current -= __n;
+        return *this;
+    }
+
+    _Self operator-(difference_type __n) const {
+        return _Self(current + __n);
+    }
+
+    _Self &operator-=(difference_type __n) {
+        current += __n;
+        return *this;
+    }
+
+    reference operator[](difference_type __n) const { return *(*this + __n); }
+};
+
+template<class _Iterator>
+inline bool operator==(const reverse_iterator<_Iterator> &__x,
+                       const reverse_iterator<_Iterator> &__y) {
+    return __x.base() == __y.base();
 }
 
-template<class RandomIterator>
-inline typename IteratorTraits<RandomIterator>::difference_type
-DistanceInnerInner(RandomIterator first, RandomIterator last, RandomAccessIteratorTag) {
-    return last - first;
+template<class _Iterator>
+inline bool operator<(const reverse_iterator<_Iterator> &__x,
+                      const reverse_iterator<_Iterator> &__y) {
+    return __y.base() < __x.base();
 }
 
-template<class InputInterator>
-inline typename IteratorTraits<InputInterator>::difference_type
-Distance(InputInterator first, InputInterator last) {
-    typedef typename IteratorTraits<InputInterator>::iterator_category category;
-    return DistanceInner(first, last, category());
+template<class _Iterator>
+inline typename reverse_iterator<_Iterator>::difference_type
+operator-(const reverse_iterator<_Iterator> &__x,
+          const reverse_iterator<_Iterator> &__y) {
+    return __y.base() - __x.base();
 }
 
-template<class BidirectionalIterator, class Distance>
-inline void AdvanceInner(BidirectionalIterator &i, Distance n, BidirectionalIteratorTag) {
-    if (n >= 0)
-        while (n--)++i;
-    else
-        while (n++)--i;
+template<class _Iterator>
+inline reverse_iterator<_Iterator>
+operator+(typename reverse_iterator<_Iterator>::difference_type __n,
+          const reverse_iterator<_Iterator> &__x) {
+    return reverse_iterator<_Iterator>(__x.base() - __n);
 }
-
-template<class InputInterator, class Distance>
-inline void AnvanceInner(InputInterator &i, Distance n, InputInteratorTag) {
-    while (n--)++i;
-}
-
-template<class RandomAccessInterator, class Distance>
-inline void AnvanceInner(RandomAccessInterator &i, Distance n, RandomAccessIteratorTag) {
-    i += n;
-}
-
-template<class InputInterator, class Distance>
-inline void Advance(InputInterator &i, Distance n) {
-    AdvanceInner(i, n, IteratorCategory(i));
-}
+//4 双向插入结束
--- STL/main.cpp
+++ STL/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Allocator.h"
+#include "iterator.h"
 class Student{
 public:
     char* name="leo";
@@ -10,58 +11,12 @@ private:
 protected:
     char * lover="NO";
 };
+
 int main() {
-
-    //*****************test*****************//
-//    std::cout << "Hello, World!" << std::endl;
-//    Allocator<int > allocator;
-//    int *p=allocator.allocate(100);
-//    int *p1=allocator.allocate(100);
-//    int *p2=allocator.allocate(100);
-//    int *p3=allocator.allocate(100);
-//    int *p4=allocator.allocate(100);
-//    int *p5=allocator.allocate(100);
-//    int *p6=allocator.allocate(100);
-//    int *p7=allocator.allocate(100);
-//     *p2=2;
-//    *p3=4;
-//    std::cout<<p<<"\n";
-//    std::cout<<p1<<"\n";
-//    std::cout<<p2<<"\n";
-//    std::cout<<p3<<"\n";
-//    std::cout<<*p<<"\n";
-//    std::cout<<*p2<<"\n";
-//    std::cout<<*p3<<"\n";
-
-
-    //*****************test*****************//
-//    Allocator<char > CharAllocator;
-//    char * CharPointer1=CharAllocator.allocate(100);
-//    char * CharPointer2=CharAllocator.allocate(1);
-//    char * CharPointer3=CharAllocator.allocate(1);
-//    char * CharPointer4=CharAllocator.allocate(1);
-//    char * CharPointer5=CharAllocator.allocate(1);
-//    char * CharPointer6=CharAllocator.allocate(1);
-//    std::cout<<&CharPointer1<<"\n";
-//    std::cout<<&CharPointer2<<"\n";
-//    std::cout<<&CharPointer3<<"\n";
-//    std::cout<<&CharPointer4<<"\n";
-//    std::cout<<&CharPointer5<<"\n";
-
-
-    //*****************test*****************//
-    Allocator<Student>StudentAllocator;
-    Student* StudentPointer1=StudentAllocator.allocate(1);
-    Student* StudentPointer2=StudentAllocator.allocate(1);
-    Student* StudentPointer3=StudentAllocator.allocate(1);
-    Student* StudentPointer4=StudentAllocator.allocate(1);
-    Student* StudentPointer5=StudentAllocator.allocate(1);
-    std::cout<<StudentPointer1<<"\n";
-    std::cout<<StudentPointer2<<"\n";
-    std::cout<<StudentPointer3<<"\n";
-    std::cout<<StudentPointer4<<"\n";
-    std::cout<<StudentPointer5<<"\n";
-    StudentAllocator.deallocate(StudentPointer1, sizeof(Student));
+    int a[100]={1,2,3,4,5,6,7,8,9,10};
+    IteratorTraits<int* >iterator;
+    Advance(iterator,6);
+    std::cout<<a[iterator.]
 
     return 0;
 }
